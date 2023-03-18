548a7f5868e81feda7ef81f34efd6044775784ff
Taha Baran <tahabarann@hotmail.com>
2023-03-18 15:17:40 +0300

    Ders 19

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,31 +14,39 @@ export class ProductComponent implements OnInit {
 
   products: Product[] = [];
   dataLoaded = false;
+  filterText = "";
 
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService: CartService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
       if (params["categoryId"]) {
-        this.getProductsByCategory(params["categoryId"])
+        this.getProductsByCategory(params["categoryId"]);
       }
       else {
-        this.getProducts()
+        this.getProducts();
       }
     })
   }
 
   getProducts() {
     this.productService.getProducts().subscribe(response => {
-      this.products = response.data
+      this.products = response.data;
       this.dataLoaded = true;
     })
   }
 
   getProductsByCategory(categoryId: number) {
     this.productService.getProductsByCategory(categoryId).subscribe(response => {
-      this.products = response.data
+      this.products = response.data;
       this.dataLoaded = true;
     })
+  }
+
+  addToCart(product: Product) {
+    this.toastrService.success("Sepete eklendi", product.productName);
+    this.cartService.addToCart(product);
   }
 }
